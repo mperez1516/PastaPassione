88173b4931b3181c8633d5a3072623200e2988c9
Mariana Perez Casallas <95313566+mperez1516@users.noreply.github.com>
2025-04-17 21:04:01 -0500

    Refactor carpeta clienteViews

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { AdminViewsModule } from './screens/adminViews/admin-views/admin-views.module';
 import { FrontPagesModule } from './screens/frontPages/front-pages/front-pages.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HistorialPedidosComponent } from './screens/clienteViews/historial-pedidos/historial-pedidos.component';
+import { PedidosActivosComponent } from './screens/clienteViews/pedidos-activos/pedidos-activos.component';
+import { EditarPerfilComponent } from './screens/clienteViews/editar-perfil/editar-perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistorialPedidosComponent,
+    PedidosActivosComponent,
+    EditarPerfilComponent
   ],
   imports: [
     BrowserModule,
